4c1a8609f1323c610037b687773f7faf628ffe28
athribis <schaeffer.aymeric@gmail.com>
2020-12-10 15:48:53 +0100

    Commit pour recuperer sur la branche POO_exemple_class_dans_main des appels au objets pour pas les recopier = flemme

--- POO/class/Personnage.cpp
+++ POO/class/Personnage.cpp
@@ -3,3 +3,30 @@
 //
 
 #include "Personnage.h"
+#include <string>
+using namespace std;
+
+void Personnage::recevoirDegats(int nbDegats) {
+    m_vie -= nbDegats; // On enlève le nombre de dégats reçus
+    if (m_vie < 0) {
+        m_vie = 0;
+    } // Evite d'avoir une vie négative
+}
+
+void Personnage::attaquer(Personnage &cible) { // référence vers un objet de type Personnage
+    cible.recevoirDegats(m_degatsArme); // On inflige les dégats de l'arme du joueur a la cible (qui recoit les dégats)
+}
+
+void Personnage::changerArme(std::string nomNouvelleArme, int degatsNouvelleArme) {
+    m_nomArme = nomNouvelleArme;
+    m_degatsArme = degatsNouvelleArme;
+}
+
+bool Personnage::estVivant() {
+    if (m_vie > 0) { // Plus que 0 de vie ?
+        return true; // Vivant
+    } else {
+        return false; // Mort
+    }
+}
+
--- POO/class/Personnage.h
+++ POO/class/Personnage.h
@@ -4,10 +4,25 @@
 
 #ifndef COURS_PERSONNAGE_H
 #define COURS_PERSONNAGE_H
+#include <string>
 
+/*
+ * Ne jamais utiliser using namespace std; au début d'un header, fichier .h
+ */
 
 class Personnage {
+public:
+    void recevoirDegats(int nbDegats);
+    void attaquer(Personnage &cible);
+    void boirePotionDeVie(int quantitePotion);
+    void changerArme(std::string nomNouvelleArme, int degatsNouvelleArme);
+    bool estVivant();
 
+private:
+    int m_vie;
+    int m_mana;
+    std::string m_nomArme;
+    int m_degatsArme;
 };
 
 
