364a1142f4d864b35b7751950f7e6bef3869b965
athribis <schaeffer.aymeric@gmail.com>
2020-12-12 13:46:11 +0100

    Création de la class Arme en cours ...

---
+++ POO/class/Arme.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Aymeric Schaeffer on 12/12/2020.
+//
+
+#include "Arme.h"
---
+++ POO/class/Arme.h
@@ -0,0 +1,14 @@
+//
+// Created by Aymeric Schaeffer on 12/12/2020.
+//
+
+#ifndef COURS_ARME_H
+#define COURS_ARME_H
+
+
+class Arme {
+
+};
+
+
+#endif //COURS_ARME_H
