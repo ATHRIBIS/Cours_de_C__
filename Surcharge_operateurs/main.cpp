2e5eba95a609b641b1da17e07482bc0c7d21931f
athribis <schaeffer.aymeric@gmail.com>
2020-12-19 16:23:40 +0100

    Surcharge d'opérateur addition d'objet

---
+++ Surcharge_operateurs/main.cpp
@@ -0,0 +1,30 @@
+//
+// Created by Aymeric Schaeffer on 19/12/2020.
+//
+#include <iostream>
+#include "class/duree.h"
+
+using namespace std;
+
+int main() {
+    /*
+     * Au lieu de faire
+     * resultat = additionner(duree1, duree2)
+     * On pourrait faire
+     * resultat = duree1 + duree2
+     * duree1 et 2 n'étant pas des nombres mais des objets
+    */
+    Duree duree1(1,60,28), duree2(0,10,61);
+    Duree resultat;
+
+    duree1.afficher();
+    cout << "+" << endl;
+    duree2.afficher();
+
+    resultat = duree1 + duree2;
+    cout << "=" << endl;
+    resultat.afficher();
+
+
+    return 0;
+}
